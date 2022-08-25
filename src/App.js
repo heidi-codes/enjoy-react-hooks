efbf5f63c3f2ccd41a6bc6078d8f0eaafbdb5f70
Heidi Codes <109416973+heidi-codes@users.noreply.github.com>
2022-08-25 12:23:08 +0800

    feat: App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./styles.css";
 
+const routes = [];
+
+const Empty = () => "";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="app">
+        <Empty />
+        <ul className="sider">
+          {routes.map(([label]) => (
+            <li>
+              <Link to={`/${label.replace(" ", "/")}`}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+        <div id="pageContainer" className="page-container">
+          <Switch>
+            {routes.map(([label, Component, additionalRoute = ""]) => (
+              <Route
+                key={label}
+                path={`/${label.replace(" ", "/")}${additionalRoute}`}
+              >
+                <Component />
+              </Route>
+            ))}
+            <Route path="/" exact>
+              <h1>Welcome!</h1>
+            </Route>
+            <Route path="*">Page not found.</Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
